74be512317279d881dbdb7d89ef0cbcec3dbb92f
KeisGSmit <kgsmit98@gmail.com>
2020-11-19 11:45:41 +0000

    Jquery added and made sure the CSS file and the Javascript files work

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("button").on("click", function() {
+        $(this).css("font-color", "#ff99ff");
+        $(this).css("background-color", "#000066");
+    });
+});
