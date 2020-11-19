8f36e7be2a064265e0288168d066d373930bddec
KeisGSmit <kgsmit98@gmail.com>
2020-11-19 11:55:17 +0000

    added materialize CSS and JS CDN in my base.html and removed the testing elements in base.html

--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $("button").on("click", function() {
-        $(this).css("font-color", "#ff99ff");
-        $(this).css("background-color", "#000066");
-    });
-});
