5ce2c344320370dfc06dd347852d52a682442180
KeisGSmit <kgsmit98@gmail.com>
2020-12-02 19:37:55 +0000

    adding in the log in function to the app

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -15,7 +16,28 @@ app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.secret_key = os.environ.get('SECRET_KEY')
 
+
 mongo = PyMongo(app)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({'username': request.form.get("username").lower()})
+        if existing_user:
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get('username').lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for('home', username=session["user"]))
+
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 @app.route("/", methods=["GET", "POST"])
