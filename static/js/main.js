0ffaa4ac8bf995a6b9484a454aaffb281be336fe
KeisGSmit <kgsmit98@gmail.com>
2020-12-04 09:35:55 +0000

    commented on the CSS, JS, HTML and 84/231 python file. Need to do more commenting

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,49 @@
 $(document).ready(function(){
+    // Materialize initialisation Javascript functions
     $('.sidenav').sidenav();
     $('.slider').slider();
     $('select').formSelect();
     $('.modal').modal();
 
-    // Function to dynamically add input fields for "Instructions" and "Ingredients"
+    // Function to dynamically add input fields for "Instructions"
     //https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
-	let max_fields_instructions	  = 50; //maximum input boxes allowed for instructions
-	let instructions_input_fields = $(".instructions_input_fields"); //Fields wrapper
-	let add_button_instructions    = $(".add_field_instructions"); //Add button ID
-    let instructions_count               = 1; //initlal text box count
+
+    //maximum input boxes allowed for instructions
+    let max_fields_instructions	  = 50; 
     
-    $(add_button_instructions).click(function(page){ //on add input button click
-		page.preventDefault();
-		if(instructions_count < max_fields_instructions){ //max input box allowed
-            instructions_count++; //text box increment
+    // Instruction fields wrapper
+    let instructions_input_fields = $(".instructions_input_fields"); 
+    
+    //Add button ID for instructions
+    let add_button_instructions    = $(".add_field_instructions"); 
+    
+    //initlal instruction text box count
+    let instructions_count         = 1; 
+    
+    //on click - add input button for instructions
+    $(add_button_instructions).click(function(page){ 
+
+        //prevents page from reloading
+        page.preventDefault();
+        
+        // appends HTML to Fields wrapper to add input boxes, ensuring not to exceed  
+		if(instructions_count < max_fields_instructions){ 
+            //text box increment
+            instructions_count++; 
             //add input box
 			$(instructions_input_fields).append('<div><input name="instructions" type="text" class="validate" required><a href="#" class="btn-small background-color-secondary-2-1 remove_field">Remove</a></div>');
 		}
 	});
-	
-	$(instructions_input_fields).on("click",".remove_field", function(e){ //user click on remove text
+    
+    // Removes the appended HTML and decreases input box count by 1
+    // User click on remove text
+	$(instructions_input_fields).on("click",".remove_field", function(e){ 
 		e.preventDefault(); $(this).parent('div').remove(); instructions_count--;
     });
     
+
+    // Function to dynamically add input fields for "Instructions"
+    //https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery   
     let max_fields_ingredients  	= 50; //maximum input boxes allowed for ingredients
     let ingredients_input_fields    = $(".ingredients_input_fields");
     let add_button_ingredients      = $(".add_field_ingredients");
@@ -41,19 +61,24 @@ $(document).ready(function(){
         e.preventDefault(); $(this).parent('div').remove(); ingredients_count--;
     });
 
+
+    // Tim Nelson's validation function for Materialize
     validateMaterializeSelect();
     function validateMaterializeSelect() {
+        //Creating variables that have custom CSS styling 
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
         let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
         if ($("select.validate").prop("required")) {
             $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
         }
+        //If there is a selected option the select drop down box will be styled with the correct CSS
         $(".select-wrapper input.select-dropdown").on("focusin", function () {
             $(this).parent(".select-wrapper").on("change", function () {
                 if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
                     $(this).children("input").css(classValid);
                 }
             });
+        // If there is data in the input box then it will be styled as valid, else it will be styled as invalid
         }).on("click", function () {
             if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
                 $(this).parent(".select-wrapper").children("input").css(classValid);
